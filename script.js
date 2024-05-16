4b2f6260b114fafd463c02f0ae73551a16f20b57
Omair Majid <omajid.dev@gmail.com>
2024-05-16 15:22:59 -0700

    chore: Update calculator display to use input element

---
+++ script.js
@@ -0,0 +1,34 @@
+let displayText = document.querySelector('.display__text')
+let numberBtn = document.querySelectorAll('.number')
+let operatorBtn = document.querySelectorAll('.operator')
+let equalBtn = document.querySelector('.btn__equal')
+let clearBtn = document.querySelector('.btn__clear')
+
+numberBtn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    if (displayText.value === '0') {
+      displayText.value = ''
+    }
+    displayText.value += btn.textContent
+  })
+})
+
+operatorBtn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    displayText.value += btn.textContent
+  })
+})
+
+equalBtn.addEventListener('click', calculate)
+
+clearBtn.addEventListener('click', clearDisplay)
+
+function clearDisplay() {
+  displayText.value = '0'
+}
+
+function calculate() {
+  let calculation = displayText.value
+  let result = eval(calculation)
+  displayText.value = result
+}
